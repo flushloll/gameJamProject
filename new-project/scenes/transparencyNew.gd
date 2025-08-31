02d9c9e2ee6411ee40ca4e54c2e40472b587fcc7
flushloll <asumaccdualagar@gmail.com>
2025-08-31 16:57:07 -0700

    working see-through roof shaders

--- new-project/scenes/transparencyNew.gd
+++ new-project/scenes/transparencyNew.gd
@@ -1,8 +1,31 @@
-extends MeshInstance3D
+extends Area3D
 
 @onready var player = $"../Player"
 
+@export var roof_shader_material: ShaderMaterial
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		print("Entered roof area")
+		get_child(0).get_child(0).get_surface_override_material(0).set_shader_parameter("player_is_under_roof", true)
+		get_child(0).get_child(0).get_surface_override_material(4).set_shader_parameter("player_is_under_roof", true)
+		get_child(0).get_child(0).get_surface_override_material(0).set_shader_parameter("STRENGTH", 0.2)
+		get_child(0).get_child(0).get_surface_override_material(4).set_shader_parameter("STRENGTH", 1)
+	else:
+		print("Not a player")
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+		get_child(0).get_child(0).get_surface_override_material(0).set_shader_parameter("player_is_under_roof", false)
+		get_child(0).get_child(0).get_surface_override_material(4).set_shader_parameter("player_is_under_roof", false)
+		print("Exited roof area, material:")
+
 func _process(delta):
+	
 	var player_screen_pos = get_viewport().get_camera_3d().unproject_position(player.global_position)
 	
 	RenderingServer.global_shader_parameter_set(
